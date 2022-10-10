fd2770936b4d67ddc07822df570c308dddd228ea
JakubKedzior <JakubKedzior17@gmail.com>
2022-10-10 17:19:49 -0500

    done

--- map.py
+++ map.py
@@ -1,13 +1,17 @@
+#takes hexadecimal input, returns rgb output
+from turtle import xcor
 
-# used to split lists into chunks
-def divide_chunks(l, n):
-     
-    # looping till length l
-    for i in range(0, len(l), n):
-        yield l[i:i + n]
+
+def hex_to_rgb(value):
+    value = value.lstrip('#')
+    lv = len(value)
+    return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
 
 #neccisary for geocoder to run, idk why
 if __name__ == '__main__':
+
+    #for math stuff
+    import math
 
     # allows program to check if a coordinate is in the US
     import reverse_geocoder
@@ -18,16 +22,19 @@ if __name__ == '__main__':
     #used for display
     import pygame
 
-    #just in case
-    import random
-    import math
-    import time
+    #takes park data and turns it into a 2d array
+    import csv
+    datafile = open('Locations.csv', 'r')
+    datareader = csv.reader(datafile, delimiter=',')
+    data = []
+    for row in datareader:
+        data.append(row)  
 
     #initializing pygame
     WHITE = (255,255,255)
     pygame.init()
 
-    #finds size of display and creates pygame window
+    #finds size of computer display and creates pygame window that is 1/4 the size
     infoObject = pygame.display.Info()
     width = int(infoObject.current_w/2)
     height = int((infoObject.current_h - 60)/2)
@@ -38,7 +45,7 @@ if __name__ == '__main__':
     #List of every pixel/coord on screen
     coordsList = []
 
-    #populating list
+    #populating list with every pixel on the display
     for i in range(width):
         for j in range(height):
 
@@ -51,35 +58,62 @@ if __name__ == '__main__':
             lat = (j * (25/(float(height)))) + 25
             long = -1 * ((i * (59/float(width))) + 66)
             coordsList.append([i,j,lat,long,0])
-    print(coordsList[10])
-    #formating to input into reverse geocoder
+
+    #formating to input into reverse geocoder by getting rid of everything except for coordinates
     rgInput = []
     for i in coordsList:
         rgInput.append((i[2],i[3]))
-    print(len(rgInput))
-    print(rgInput[10])
-    #adds extra variable to the coordinate tuple that defines its color based off of wether or not its in the US
+
+    #adds extra variable to the coordList that defines if it is part of the continental US
     results = reverse_geocoder.search(rgInput)
-    print(len(results))
-    print(results[10])
     for i in range(len(results)):
         if(globe.is_land(coordsList[i][2],coordsList[i][3]) and results[i]['cc'] == "US"):
             coordsList[i][4] = 1
-        print(str(int((i/len(results))*100)) + "% done")
-    #creates display
-    print(coordsList[10])
+
+    #creates display by iterating through the pixels of the screen and coloring by comparison to the csv
     for i in coordsList:
         if i[4] == 1:
-            pygame.draw.rect(DISPLAY,(0,0,0),((-1 * i[0]) + (width),(-1 * i[1]) + (height),1,1))
+            #iterating through parks to see which one is closest
+            color = "#000000"
+            closest = 9999999
+            for j in data:
+                #calculates the distance between the park and pixel, usingthe numhber of coasters as a weight
+                distance = (1/float(j[1])) * math.dist((float(j[2]),float(j[3])),(i[2],i[3]))
+                if distance < closest:
+                    color = j[4]
+                    closest = distance
+            pygame.draw.rect(DISPLAY,hex_to_rgb(color),((-1 * i[0]) + (width),(-1 * i[1]) + (height),1,1))
             pygame.display.update()
 
     print('done')
 
     while(True):
-
+        
         #allows user to quit if program is taking to long
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-
+        (x,y) = pygame.mouse.get_pos()
+        x = -x + width
+        y = -y + height
+        y = (y * (25/(float(height)))) + 25
+        x = -1 * ((x * (59/float(width))) + 66)
+        name = "none"
+        closest = 9999999
+        for j in data:
+                #calculates the distance between the park and pixel, usingthe numhber of coasters as a weight
+                distance = (1/float(j[1])) * math.dist((float(j[2]),float(j[3])),(y,x))
+                if distance < closest:
+                    name = str(j[0])
+                    closest = distance
+        
+        print(name)
+        name = name + ' ' * (60 - len(name))
+        text = pygame.font.Font('freesansbold.ttf', 20).render(name, True, (0,0,0), (255,255,255))
+ 
+        textRect = text.get_rect()
+        textRect.left = 0
+        textRect.bottom = height
+        DISPLAY.blit(text, textRect)
         pygame.display.update()
+    
