9df7d7ccff23bcfb0ae3b8f2d7e736e80d720151
JakubKedzior <JakubKedzior17@gmail.com>
2022-10-09 21:20:32 -0500

    working map with csv

--- map.py
+++ map.py
@@ -8,8 +8,12 @@ def divide_chunks(l, n):
 
 #neccisary for geocoder to run, idk why
 if __name__ == '__main__':
+
     # allows program to check if a coordinate is in the US
     import reverse_geocoder
+
+    #allows for the program to check if a location is on land
+    from global_land_mask import globe
 
     #used for display
     import pygame
@@ -25,7 +29,9 @@ if __name__ == '__main__':
 
     #finds size of display and creates pygame window
     infoObject = pygame.display.Info()
-    DISPLAY = pygame.display.set_mode((infoObject.current_w, infoObject.current_h - 60), 0, 32)
+    width = int(infoObject.current_w/2)
+    height = int((infoObject.current_h - 60)/2)
+    DISPLAY = pygame.display.set_mode((width, height), 0, 32)
     pygame.display.set_caption('Coaster Map')
     DISPLAY.fill(WHITE)
 
@@ -33,8 +39,8 @@ if __name__ == '__main__':
     coordsList = []
 
     #populating list
-    for i in range(infoObject.current_w):
-        for j in range(infoObject.current_h - 60):
+    for i in range(width):
+        for j in range(height):
 
             #allows user to quit if program is taking to long
             for event in pygame.event.get():
@@ -42,56 +48,30 @@ if __name__ == '__main__':
                     pygame.quit()
 
             #translating pixels to coordinates
-            long = (j * (25/(infoObject.current_h - 60))) + 25
-            lat = (i * (59/infoObject.current_w)) + 66
-
+            lat = (j * (25/(float(height)))) + 25
+            long = -1 * ((i * (59/float(width))) + 66)
             coordsList.append([i,j,lat,long,0])
-
+    print(coordsList[10])
     #formating to input into reverse geocoder
     rgInput = []
-    rgChunk = []
-    counter = 1800
-    print(coordsList[20000])
-    time.sleep(100)
     for i in coordsList:
-        if counter <= 0:
-            rgInput.append(rgChunk)
-            rgChunk = []
-            counter = 1800
-        counter -=1
-        rgChunk.append((i[2],i[3]))
-
-
+        rgInput.append((i[2],i[3]))
+    print(len(rgInput))
+    print(rgInput[10])
     #adds extra variable to the coordinate tuple that defines its color based off of wether or not its in the US
-    results =[]
-    counter = 0
-    for i in rgInput:
-        #allows user to quit if program is taking to long
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-        resultChunk = reverse_geocoder.search(i)
-        results = results + resultChunk
-
-        counter +=1
-        print(str((int((counter/len(rgInput)) * 100)))+ "% done")
-
-
+    results = reverse_geocoder.search(rgInput)
+    print(len(results))
+    print(results[10])
     for i in range(len(results)):
-        if(results[i]['cc'] == "US"):
+        if(globe.is_land(coordsList[i][2],coordsList[i][3]) and results[i]['cc'] == "US"):
             coordsList[i][4] = 1
-        else:
-            coordsList[i][4] = 0
-
+        print(str(int((i/len(results))*100)) + "% done")
     #creates display
-    counter = 0
+    print(coordsList[10])
     for i in coordsList:
         if i[4] == 1:
-            pygame.draw.rect(DISPLAY,(0,0,0),(i[1],i[2],1,1))
+            pygame.draw.rect(DISPLAY,(0,0,0),((-1 * i[0]) + (width),(-1 * i[1]) + (height),1,1))
             pygame.display.update()
-            print(i)
-            counter += 1
-            print(counter)
 
     print('done')
 
